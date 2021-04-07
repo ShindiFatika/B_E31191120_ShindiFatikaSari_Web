104f2d51cf631a0d4147254758107d87e42feafd
Shindi Fatika Sari <66682288+ShindiFatika@users.noreply.github.com>
2021-04-07 09:58:24 +0700

    E31191120_ShindiFatikaSari

---
+++ Minggu9/application/models/Hello_model.php
@@ -0,0 +1,13 @@
+<?php
+class Hello_model extends CI_Model{
+    //public $txt = 'Hello World!';
+    
+    public $alert = 'Hello Wolrd dari CI Model';
+    
+    public $mahasiswa = [
+			["nama"=>"Adya","nim"=>"E31190957","gol"=>"B"],
+			["nama"=>"Shindi","nim"=>"E31191120","gol"=>"B"] 
+			["nama"=>"Vival ","nim"=>"E31190785","gol"=>"A"],
+
+		];
+}
